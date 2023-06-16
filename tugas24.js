0a99e143accb228cbc50f2fd9fc5cf3732d05e6a
arif wijanarko <arifpethod@gmail.com>
2023-06-16 09:28:23 +0700

    tugas javascript 24
    untuk niomic

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+
+    var deret = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+    
+    console.log("Sebelumnya : ", deret.toString());
+
+    var asc = deret.sort();
+    console.log("Ascending : ", asc.toString());
+    var desc = asc.reverse();
+    console.log("Descending : ", desc.toString());
